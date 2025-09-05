6a535e831974689b3982006db608d90496a5378e
omkark1558-byte <omkark1558>
2025-09-05 15:23:22 +0530

    Nonprimitive Program

---
+++ Second/src/Datatype/Noprimitive.java
@@ -0,0 +1,38 @@
+package Datatype;
+
+public class Noprimitive {
+
+	public static void main(String[] args) 
+	{
+		
+	 int [] arr=new int[6];
+	 arr[0]=10;
+	 arr[1]=50;
+	 arr[2]=60;
+	 arr[3]=56;
+	 arr[4]=58;
+	 arr[5]=89;
+	 
+	System.out.println(arr);
+	 
+	 char[] single=new char[6];
+	 
+	 single[0]='O';
+	 single[1]='M';
+	 single[2]='K';
+	 single[3]='A';
+	 single[4]='R';
+	 single[5]='K';
+	 
+//	 System.out.println(single);
+	 
+	 String[] Names=new String[2];
+	 Names[0]="Omkar";
+	 Names[1]="kale";
+	 
+	 System.out.println(Names[0]+Names[1]);
+
+	 //System.out.println(Names[0]);
+	 //System.out.println(Names[1]);
+	}
+}
