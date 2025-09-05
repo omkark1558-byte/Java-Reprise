92f487b8b13e0b45539f6668d406ff8a6113c6b6
omkark1558-byte <omkark1558>
2025-09-05 15:07:55 +0530

    Casting program

---
+++ Second/src/Datatype/Casting.java
@@ -0,0 +1,25 @@
+package Datatype;
+//Source code is decompiled from a .class file using FernFlower decompiler
+
+public class Casting {
+public Casting() {
+}
+
+public static void main(String[] args) {
+   double d = 12.25;
+   
+   System.out.println(d);
+   
+   int i = 45;
+   
+   System.out.println(i);
+   
+   int J = 70;
+   
+   System.out.println(i);
+   
+   double H = 45.0;
+   
+   System.out.println(d);
+}
+}
