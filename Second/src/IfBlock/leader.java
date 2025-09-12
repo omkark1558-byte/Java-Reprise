bedbd91787de7481354045328a5c24b59373012f
omkark1558-byte <omkark1558>
2025-09-12 15:46:21 +0530

    Ifblock Program

---
+++ Second/src/IfBlock/leader.java
@@ -0,0 +1,32 @@
+package IfBlock;
+//import java.util.Scanner;
+
+public class leader {
+
+	public static void main(String[] args){
+		
+		//Scanner scanner=new Scanner(System.in);
+		
+		double price=65.25666;
+	
+		if(price>0 && price<=55)
+		{
+			System.out.println("You Purces Book");
+		}
+		else if(price>55 && price<=65)
+		{
+			System.out.println("Medium Purches Range Book ");
+		}
+		else if(price>65 && price<=75)
+		{
+			System.out.println("High Price Book");
+		}
+		else if(price>75 && price <=100)
+		{
+			System.out.println("High Price Of Book Plz Buying Book Yor Renspobility");
+		}
+		else {
+			System.out.println("Got It Any More Shop");
+		}
+	}
+}
