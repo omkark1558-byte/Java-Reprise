626388c1e67829986b5bb31a1e4730114b12a7ac
omkark1558-byte <omkark1558@gmail.com>
2026-02-09 21:34:52 +0530

    Logicla's....

--- new_package/src/Encapsoulation/Encap_02.java
+++ new_package/src/Encapsoulation/Encap_02.java
@@ -2,32 +2,28 @@ package Encapsoulation;
 
 public class Encap_02 {
 
-	public static void main(String[] args)	{
-		 
-		
-			student02 Omii=new student02();
-		        Omii.Name();
-		        Omii.Student_Id();
-		    }
-		    
-		}
-		class Student
-		{
-		    public void Name()
-		    {
-		        System.out.println("Student Name Is : Xyz");
-		    }
+	public static void main(String[] args) {
 
-		}
-		class student01 extends Student
-		{
-		    public void Student_Id()
-		    {
-		        System.out.println("Student id is : xyz @ 123");
-		    }
-		}
-		class student02 extends student01
-		{
+		student02 Omii = new student02();
+		Omii.Name();
+		Omii.Student_Id();
+	}
 
-		}
-	
+}
+
+class Student {
+	public void Name() {
+		System.out.println("Student Name Is : Xyz");
+	}
+
+}
+
+class student01 extends Student {
+	public void Student_Id() {
+		System.out.println("Student id is : xyz @ 123");
+	}
+}
+
+class student02 extends student01 {
+
+}
--- new_package/src/Encapsoulation/Encap_03.java
+++ new_package/src/Encapsoulation/Encap_03.java
@@ -1,28 +1,25 @@
 package Encapsoulation;
 
 public class Encap_03 {
-	public static void main(String args[])
-	{
-		
-		car c=new car();
+	public static void main(String args[]) {
+
+		car c = new car();
 		c.Ford(450000.00);
 		c.Ford(45.00, "Caar");
 		c.Ford(12, 020, 015.0);
 	}
 }
 
-class car
-{
-	public void Ford(double p)
-	{
+class car {
+	public void Ford(double p) {
 		System.out.println("Car : A+");
 	}
-	public void Ford(double p, String t)
-	{
+
+	public void Ford(double p, String t) {
 		System.out.println("Car Is : Legender Plus");
 	}
-	public void Ford(float y,int i,double j)
-	{
+
+	public void Ford(float y, int i, double j) {
 		System.out.println("Car Is : Fortuner:(F)");
 	}
 }
